7bc57fe35d7a50ad7000aa042400f4d1948216b2
anjana <kad.an.devi@gmail.com>
2024-04-14 13:14:44 +0530

    check if given 2 BT's are same or not

--- Trees/same_tree.py
+++ Trees/same_tree.py
@@ -12,10 +12,10 @@ class TreeNode:
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         def dfs(tree1, tree2):
-            if tree1 is None or tree2 is  None:#implies if tree1 is none and tree2 is not and viceversa , also cover case when both values are not same
-                if tree1 == tree2:
-                    return True
-                else:
-                    return False
+            if tree1 is None and tree2 is None:
+                return True
+            if tree1 is None or tree2 is None:
+                return False
+            #tree1 and tree2 not empty
             return tree1.val == tree2.val and dfs(tree1.left, tree2.left) and dfs(tree1.right, tree2.right)
         return dfs(p,q)
