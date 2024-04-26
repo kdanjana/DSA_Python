1abb7162b3dca260e98efe02167e66b139da3787
anjana <kad.an.devi@gmail.com>
2024-04-26 12:46:31 +0530

    bit manipulation

---
+++ BitManipulation/count_set_bits.py
@@ -0,0 +1,10 @@
+def numberOfSetBits(n):
+    count = 0
+    while n > 0:
+        count += n & 1
+        n = n >> 1
+    return count
+
+if __name__ == "__main__":
+    n = int(input("Enter number: "))
+    print(f"Number of set bits in {n} are {numberOfSetBits(n)}")
